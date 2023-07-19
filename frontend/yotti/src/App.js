7a2b9bdf1a32d6c1174840ff4395de2d1d981a72
famak <famaki310@gmail.com>
2023-07-19 21:56:40 +0300

    Nav bar created

--- frontend/yotti/src/App.js
+++ frontend/yotti/src/App.js
@@ -1,28 +1,21 @@
 import "./index.css";
 
+import Nav from "./components/navigation/Nav";
+
 function App() {
   return (
-    <div className="flex gap-11">
-      <div className="ml-18 container m-10  rounded-xl border bg-gray-200 p-8 shadow">
-        <Header />
-      </div>
+    <div className=" w-full">
+      <Nav />
     </div>
   );
 }
 
-function Header() {
-  return (
-    <div className="header">
-      <p className="text-grey-700 font-blond mb-5 text-3xl">Welcome!</p>
-      <p className="text-grey-500 text-lg">React and Tailwind CSS in action</p>
-      <ul>
-        <li>
-          <img src="img1.png" alt="img" />
-        </li>
-        <li>This project is in production</li>
-      </ul>
-    </div>
-  );
-}
+// function Loader() {
+//   return (
+//     <div className="absolute inset-0 flex items-center justify-center bg-slate-200/20 backdrop-blur-sm">
+//       <div className="loader"></div>
+//     </div>
+//   );
+// }
 
 export default App;
